4733f337fea3996cc85315ebda94c57a155ec064
Franck <117032616+franck38410@users.noreply.github.com>
2023-08-16 09:12:00 +0200

    Update constants.ts
    contrat sur mumbai

--- config/constants.ts
+++ config/constants.ts
@@ -1,5 +1,5 @@
 export const apiKeyAlchemyProvider = 'Y3mDOtUzfEkHU8KaxuyTHKncgaR5Xibb';
-export const contractAddress = '0x3e063382d0F2AFb43A75a0FE2283c64086Cc8C86';
+export const contractAddress = '0xA14766Ac3A0b83713941109B73D9ebe4Dbfaee4D';
 export const formAcadeeAddress = '0x005D71CA579843a1C3EeFEd02E5909CF77976761';
 export const formAcadeeAddress2 = '0x7Cd33a833dC720Acc8d17bC17edC41cc526FebB2';
 export const openseaUrl = 'https://testnets.opensea.io/fr/account';
