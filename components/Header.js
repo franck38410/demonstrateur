36671d4b773c19f00ac3cf3a9b594dd6af8645de
Franck <117032616+franck38410@users.noreply.github.com>
2023-09-05 10:12:06 +0200

    Add files via upload

--- components/Header.js
+++ components/Header.js
@@ -2,19 +2,48 @@
 import { Flex, Image } from '@chakra-ui/react';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import ActiveLink from 'components/ActiveLink'
-import { useAccount } from 'wagmi'
+import { useAccount, useProvider } from 'wagmi'
+import { ethers } from 'ethers';
+import { useState, useEffect } from 'react';
+import Contract from '/config/Demonstrateur.json';
+import { contractAddress } from 'config/constants';
 
 export default function Header() {
-    const { isConnected } = useAccount()
+    const provider = useProvider();
+    const { address, isConnected } = useAccount();
+    const [nom, setNom] = useState(null);
+    useEffect(() => {
+        if(isConnected) {
+          getDatas();
+        }
+      }, [])
+    
+      const getDatas = async() => {
+        console.log("getDatas address : "+address);
+        const contract = new ethers.Contract(contractAddress, Contract.abi, provider);
+        setNom(await contract.getNomClientByAddress(address));
+      }
+
     return (
         <Flex height="15vh" justifyContent="space-between" alignItems="center" p="2rem">
             <Image src='/logo.ico' width='100px' height='60px' alt='Logo' />
             {(isConnected ? (
                 <Flex>
                     < ActiveLink children="Home" href="/" />
+                    {nom}
+                    {nom =='Owner' ? (
+                        < ActiveLink children="Client" href="/client" />
+                    ) : (
+                        ""
+                    )}
+                    < ActiveLink children="Client" href="/client" />
                     < ActiveLink children="Expédition" href="/expedition" />
                     < ActiveLink children="Tableau de suivi" href="/tableau" />
-                    < ActiveLink children="Réception" href="/reception" />
+                    {nom !=='Inconnu' ? (
+                        < ActiveLink children="Réception" href="/reception" />
+                    ) : (
+                        ""
+                    )}
                     < ActiveLink children="Mes Matériels" href="/mesmateriels" />
                     < ActiveLink children="Historique" href="/historique" />
                 </Flex>
