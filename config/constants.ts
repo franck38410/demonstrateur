9f947f354f6397e22c44c9434cd7325ea051a626
Franck <117032616+franck38410@users.noreply.github.com>
2023-09-05 15:55:35 +0200

    Add files via upload

--- config/constants.ts
+++ config/constants.ts
@@ -1,5 +1,5 @@
 export const apiKeyAlchemyProvider = 'Y3mDOtUzfEkHU8KaxuyTHKncgaR5Xibb';
-export const contractAddress = '0xe9e84eb586bDC01DB5E0717B1419167d2d2708CE';
+export const contractAddress = '0x74dF61c803f6399DAe4E492bD860974ed1aAE2a1';
 export const formAcadeeAddress = '0x005D71CA579843a1C3EeFEd02E5909CF77976761';
 export const formAcadeeAddress2 = '0x7Cd33a833dC720Acc8d17bC17edC41cc526FebB2';
 export const openseaUrl = 'https://testnets.opensea.io/fr/account';
