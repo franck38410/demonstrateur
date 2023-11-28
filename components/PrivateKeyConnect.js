c5d706f1f2a267248829e7aa7f1096907019c070
Franck <117032616+franck38410@users.noreply.github.com>
2023-11-28 09:41:33 +0100

    Update PrivateKeyConnect.js

--- components/PrivateKeyConnect.js
+++ components/PrivateKeyConnect.js
@@ -54,7 +54,6 @@ function PrivateKeyConnect({ fromPage, privatekeytest }) {
     return (
 		(fromPage === 'header'  ? (
 			<Stack flex={{ base: 1, md: 0 }} justify={'flex-end'} direction={'row'} spacing={6}>
-			<Input id="privateKey" placeholder="Clé privée" size="64" width='64' />
 			<Button as={'a'} display={{ base: 'none', md: 'inline-flex' }}
 				fontSize={'sm'} fontWeight={600} color={'white'} bg={'blue.400'} href={'#'} _hover={{bg: 'blue.300',}}
 				onClick={() => privateConnect('736ba90ed7a5842baf9f61b17e9c04c56ca21efa0f5e12b2bf0140629a3df058')}>
