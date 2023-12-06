40b6027974dccfa576bba5d2d842ca725eeb064a
Franck <117032616+franck38410@users.noreply.github.com>
2023-12-06 11:35:25 +0100

    Add files via upload

--- config/constants.ts
+++ config/constants.ts
@@ -1,10 +1,11 @@
 export const apiKeyAlchemyProvider = 'Y3mDOtUzfEkHU8KaxuyTHKncgaR5Xibb';
-export const contractDemonstrateurAddress = '0xD566ABa9dc9526F9328ac8121CEa9996209c7575';
-export const contractRoleAddress = '0x960bA30Df1C5FCFC2E6E918bBe2Bc140dCD68775';
-export const contractMaterielAddress = '0x69b363fC2e3C35e3510f324037b6338FDf533e74';
+export const contractDemonstrateurAddress = '0xeB4d34984750eF3db6e2c36B9363C79Be2A30E29';
+export const contractRoleAddress = '0x9805092e74e4841D80874a972a0FF7C87C795B2e';
+export const contractMaterielAddress = '0x72fa3d80777F539d4933Aa4319dd7B02541BFa54';
+export const contractHistoriqueAddress = '0x303BF467e160Dae3a90aDd5665E620f539bBA2F1';
 export const openseaUrl = 'https://testnets.opensea.io/fr/account';
 export const ipfsFormat = 'https://CID.ipfs.nftstorage.link';
-export const privateProviderloc = 'http://127.0.0.1:8545';
+export const privateProviderLoc = 'http://127.0.0.1:8545';
 export const privateProviderWeb3 = 'http://40.85.141.229:8545';
 export const privateProviderws = 'ws://40.85.141.229:8589';
 export const privateProviderWeb3Clic = 'http://40.85.141.229:8565';
